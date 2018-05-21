8e4b18e15a8a2f8445005bacfe423281285e3691
Jakrin Juangbhanich <juangbhanich.k@gmail.com>
2018-05-21 12:19:17 +0800

    Updated Region

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# tools
-A bunch of my common used Python tools.
+# Tools
+Some  good shit to make life easier.
+
+
+
+## Wish List
+
+- [ ] Color palette (color map?) for data science and visualization.
+- [ ] Network and performance diagnostics.
+- [ ] A good system to label/write text into an image.
--- util/region.py
+++ util/region.py
@@ -2,44 +2,37 @@
 
 """
 tools| region | 20/04/18
-Region is a rect with extended utility. It has not only its edges as attributes, but also central xy, and size.
-The user can adjust any of these values through the setters, and all the other attributes will update accordingly.
-Regions can also be nested to be relative to other regions.
+A region is essentially a rectangle with some extra functionality. I've made it so the attributes will update each other.
 """
-
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
 
 __author__ = "Jakrin Juangbhanich"
 __email__ = "juangbhanich.k@gmail.com"
-__version__ = "0.0.0"
-
-
-# ======================================================================================================================
-# Region Rect (Utility) ------------------------------------------------------------------------------------------------
-# ======================================================================================================================
+__version__ = "1.0.0"
 
 
 class Region:
     def __init__(self, left=0, right=0, top=0, bottom=0):
 
         # Rect. Origin (0, 0) is top-left.
-        self.left = 0
-        self.right = 0
-        self.top = 0
-        self.bottom = 0
+        self._left = 0
+        self._right = 0
+        self._top = 0
+        self._bottom = 0
 
         # Position
-        self.x = 0
-        self.y = 0
+        self._x = 0
+        self._y = 0
 
         # Scale
-        self.width = 0
-        self.height = 0
+        self._width = 0
+        self._height = 0
 
         # Initialize
         self.set_rect(left, right, top, bottom)
+
+    # ===============================================================================================================================
+    # Functions to set the attributes explicitly.
+    # ===============================================================================================================================
 
     def set_rect(self, left, right, top, bottom):
         if right < left:
@@ -48,49 +41,136 @@ class Region:
         if bottom < top:
             raise Exception("Invalid Input", "Bottom ({}) must be greater than top ({}).".format(top, bottom))
 
-        self.left = left
-        self.right = right
-        self.top = top
-        self.bottom = bottom
+        self._left = left
+        self._right = right
+        self._top = top
+        self._bottom = bottom
         self._calibrate_to_rect()
 
     def set_xy(self, x=None, y=None):
         if x is not None:
-            self.x = x
+            self._x = x
         if y is not None:
-            self.y = y
+            self._y = y
         self._calibrate_to_xy()
 
     def set_size(self, width, height):
-        self.width = width
-        self.height = height
+        self._width = width
+        self._height = height
         self._calibrate_to_xy()
 
-    # ---------------------------------------------------------------------------
+    # ======================================================================================================================
+    # Utility functions.
+    # ======================================================================================================================
 
-    def _calibrate_to_rect(self):
-        self.width = self.right - self.left
-        self.height = self.bottom - self.top
-        self.x = self.left + self.width // 2
-        self.y = self.top + self.height // 2
-
-    def _calibrate_to_xy(self):
-        half_width = self.width // 2
-        half_height = self.height // 2
-        self.left = self.x - half_width
-        self.right = self.x + half_width
-        self.top = self.y - half_height
-        self.bottom = self.y + half_height
-
-    def contains(self, x, y):
-        if x < self.left or x > self.right or y < self.top or y > self.bottom:
+    def contains(self, x, y) -> bool:
+        """" Checks if the given x, y position is within the area of this region. """
+        if x < self._left or x > self._right or y < self._top or y > self._bottom:
             return False
         return True
 
-    def is_in_bounds(self, width, height):
-        if self.top < 0 \
-                or self.bottom > height \
-                or self.left < 0 \
-                or self.right > width:
+    def is_in_bounds(self, width, height) -> bool:
+        """ Check if this entire region is contained within the bounds of a given stage size."""
+        if self._top < 0 \
+                or self._bottom > height \
+                or self._left < 0 \
+                or self._right > width:
             return False
         return True
+
+    # ======================================================================================================================
+    # Private calibration functions.
+    # ======================================================================================================================
+
+    def _calibrate_to_rect(self) -> None:
+        self._width = self._right - self._left
+        self._height = self._bottom - self._top
+        self._x = self._left + self._width // 2
+        self._y = self._top + self._height // 2
+
+    def _calibrate_to_xy(self) -> None:
+        half_width = self._width // 2
+        half_height = self._height // 2
+        self._left = self._x - half_width
+        self._right = self._x + half_width
+        self._top = self._y - half_height
+        self._bottom = self._y + half_height
+
+    # ======================================================================================================================
+    # Property setters for our attributes.
+    # ======================================================================================================================
+
+    @property
+    def x(self):
+        return self._x
+
+    @x.setter
+    def x(self, value):
+        self._x = value
+        self._calibrate_to_xy()
+
+    @property
+    def y(self):
+        return self._y
+
+    @y.setter
+    def y(self, value):
+        self._y = value
+        self._calibrate_to_xy()
+
+    @property
+    def left(self):
+        return self._left
+
+    @left.setter
+    def left(self, value):
+        self._left = value
+        self._calibrate_to_rect()
+
+    @property
+    def right(self):
+        return self._right
+
+    @right.setter
+    def right(self, value):
+        self._right = value
+        self._calibrate_to_rect()
+
+    @property
+    def top(self):
+        return self._top
+
+    @top.setter
+    def top(self, value):
+        self._top = value
+        self._calibrate_to_rect()
+
+    @property
+    def bottom(self):
+        return self._bottom
+
+    @bottom.setter
+    def bottom(self, value):
+        self._bottom = value
+        self._calibrate_to_rect()
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        self._width = value
+        self._calibrate_to_xy()
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        self._height = value
+        self._calibrate_to_xy()
+    
+
+
