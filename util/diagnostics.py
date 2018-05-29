22b9e6dee4b203d0475694574cf59a6523bfd064
Jakrin Juangbhanich <juangbhanich.k@gmail.com>
2018-05-29 13:54:21 +0800

    Updated diagnostics logic

--- util/diagnostics.py
+++ util/diagnostics.py
@@ -4,9 +4,9 @@
 recorder-node | diagnostics | 22/05/18
 <Description>
 """
-import json
 
-from tools.util.logger import Logger
+import json
+from .logger import Logger
 
 __author__ = "Jakrin Juangbhanich"
 __copyright__ = "Copyright 2018, GenVis Pty Ltd."
@@ -15,12 +15,8 @@ __email__ = "juangbhanich.k@gmail.com"
 
 def show_size(name, obj):
 
-    if type(obj) is str:
-        encoded_str = json.dumps(obj)
-        obj_size = len(encoded_str.encode("utf-8"))
-    else:
-        obj_size = len(obj)
-
+    encoded_str = json.dumps(obj)
+    obj_size = len(encoded_str.encode("utf-8"))
     readable_size = sizeof_fmt(obj_size)
     Logger.log_field("Size of {}".format(name), readable_size)
 
