5e1fca56eaa22b338ace7e469707384337044369
Jakrin Juangbhanich <juangbhanich.k@gmail.com>
2018-06-11 13:58:32 +0800

    Renamed for clarity

--- util/pather.py
+++ util/pather.py
@@ -17,21 +17,21 @@ def create(path, clear=False):
     Given some arbitrary path, we will create all the directory leading up to it.
     Optionally, we can also clear whatever is there (only at the terminal path).
     """
-    _create(path, clear, is_head=True)
+    _create(path, clear, is_true_tail=True)
 
 
-def _create(path, clear, is_head):
+def _create(path, clear, is_true_tail):
     head, tail = os.path.split(path)
 
     # Recursively create the heads.
     if head != "":
-        _create(head, clear, is_head=False)
+        _create(head, clear, is_true_tail=False)
 
     base, ext = os.path.splitext(tail)
     if ext == "":
         # No extension, this is supposed to be a directory.
         # Clear this if it is the 'head' of the path we are creating.
-        if os.path.exists(path) and clear and is_head:
+        if os.path.exists(path) and clear and is_true_tail:
             if path != ".":  # Do NOT attempt to remove the root directory.
                 shutil.rmtree(path)
 
