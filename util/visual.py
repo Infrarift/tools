3b19d83e107ae34fedee5d3b0a49a2d7ccd68c58
Jakrin Juangbhanich <juangbhanich.k@gmail.com>
2018-06-28 17:51:20 +0800

    Added Text Visual

--- util/visual.py
+++ util/visual.py
@@ -3,10 +3,13 @@
 """
 Library to do some cool visual stuff.
 """
+from typing import List, Tuple
 
 import cv2
 import numpy as np
 import colorsys
+
+from tools.util.region import Region
 
 __author__ = "Jakrin Juangbhanich"
 __email__ = "juangbhanich.k@gmail.com"
@@ -17,7 +20,7 @@ __email__ = "juangbhanich.k@gmail.com"
 # ======================================================================================================================
 
 
-def generate_colors(n, saturation: float=1.0, brightness: float=1.0):
+def generate_colors(n, saturation: float = 1.0, brightness: float = 1.0):
     """ Generate N amount of colors spread across a range on the HSV scale.
     Will return it in a numpy format. """
     hsv = [(i / n, saturation, brightness) for i in range(n)]
@@ -26,7 +29,75 @@ def generate_colors(n, saturation: float=1.0, brightness: float=1.0):
     colors *= 255
     return colors
 
+
 # TODO: Wish list - Create a colormap system like in CV2, or adapt it so I can create my own color maps.
+
+# ======================================================================================================================
+# Region Drawing Tools.
+# ======================================================================================================================
+
+
+def draw_regions(image: np.array,
+                 regions: List[Region],
+                 color=(255, 255, 255),
+                 thickness: int=2,
+                 overlay: bool=False,
+                 strength: float=1.0):
+
+    target_image = image
+
+    if overlay:
+        overlay_image = np.zeros_like(image, np.uint8)
+        target_image = overlay_image
+
+    for i in range(len(regions)):
+        r = regions[i]
+        cv2.rectangle(target_image, (r.left, r.top), (r.right, r.bottom),
+                      color=color,
+                      thickness=thickness)
+
+    if overlay:
+        image = cv2.addWeighted(image, 1.0, overlay_image, strength, 0.0)
+
+    return image
+
+
+def pixelate_region(image: np.array, regions: Region):
+    blur_factor = 0.1
+
+    for r in regions:
+        target_image = image[r.top:r.bottom, r.left:r.right]
+        h = target_image.shape[0]
+        w = target_image.shape[1]
+
+        pixel_h = max(1, int(h * blur_factor))
+        pixel_w = max(1, int(w * blur_factor))
+
+        target_image = cv2.resize(target_image, (pixel_w, pixel_h),
+                                  interpolation=cv2.INTER_NEAREST)
+        target_image = cv2.resize(target_image, (w, h), interpolation=cv2.INTER_NEAREST)
+        image[r.top:r.bottom, r.left:r.right] = target_image
+
+    return image
+
+
+def draw_region_mask(image: np.array, regions: List[Region], strength: float = 1.0):
+    """ Apply a mask to the areas covered by the regions. """
+
+    image = image.astype(np.float)
+    pos_mask = np.zeros((image.shape[0], image.shape[1]), dtype=np.bool)
+    neg_mask = np.ones((image.shape[0], image.shape[1]), dtype=np.bool)
+
+    for r in regions:
+        pos_mask[r.top:r.bottom, r.left:r.right] = True
+        neg_mask[r.top:r.bottom, r.left:r.right] = False
+
+    fade_factor = 1.0 - (0.7 * strength)
+    print(fade_factor)
+    image[neg_mask] *= fade_factor
+    image = image.astype(np.uint8)
+    return image
+    pass
 
 
 # ======================================================================================================================
@@ -53,6 +124,13 @@ def draw_bar_segment(image, p_start: float, p_end: float, x: int, y: int, width:
 
 
 # ======================================================================================================================
+# Advanced Text.
+# ======================================================================================================================
+
+def create_v2_text_box():
+    pass
+
+# ======================================================================================================================
 # Text Tools.
 # ======================================================================================================================
 
@@ -67,7 +145,7 @@ def create_text_box(image: np.array,
                     text: str,
                     x: int, y: int, width: int, height: int,
                     text_color=(255, 255, 255), bg_color=(0, 0, 0),
-                    centered: bool=False):
+                    centered: bool = False):
     """ Create a text box, with the text written in the center."""
 
     # Find the corners of the box that we want to draw.
@@ -97,4 +175,3 @@ def create_text_box(image: np.array,
 
     # Add the text, centered to the area.
     cv2.putText(image, text, (tx, ty), font, 1, text_color)
-
