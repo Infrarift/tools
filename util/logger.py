e6cba76252b1f86aedc8b3bf351706802493afe6
Jakrin Juangbhanich <juangbhanich.k@gmail.com>
2018-08-02 17:25:32 +0800

    Upgraded Logger

--- util/logger.py
+++ util/logger.py
@@ -4,30 +4,20 @@
 Logger Description.
 """
 
+import os
 import time
 import sys
+from typing import List
+from tools.util import pather
 
 __author__ = "Jakrin Juangbhanich"
 __email__ = "juangbhanich.k@gmail.com"
 
 
-def non_minimal(f):
-    """ Decorator for a logging function that only executes when the logger is not in minimal mode. """
-    def func_wrapper(*args, **kwargs):
-        if Logger.get_instance().minimal_mode:
-            return args[1]
-        return f(*args, **kwargs)
-    return func_wrapper
-
-
 class Logger:
-
-    # TODO: Use a decorator for the minimal checking?
-
     # Character constants (tab, ruler, etc).
     INDENT_CHAR = "   "
     RULER_CHAR = "-"
-    BLANK_TAG = "   "
     PROGRESS_BLANK = "▯"
     PROGRESS_FILL = "▮"
 
@@ -36,238 +26,272 @@ class Logger:
     GREEN = '\33[32m'
     YELLOW = '\33[33m'
     BLUE = '\33[34m'
-    BOLD = '\33[5m'
     DEFAULT_COLOR = '\33[0m'
+    COLORS: List[str] = [RED, GREEN, YELLOW, BLUE, DEFAULT_COLOR]
 
     # Singleton instance.
     _INSTANCE = None
 
-    # ==================================================================================================================
-    # Public Interface -------------------------------------------------------------------------------------------------
-    # ==================================================================================================================
+    # ======================================================================================================================
+    # Reflected Static Methods.
+    # ======================================================================================================================
 
     @staticmethod
-    def get_instance() -> "Logger":
+    def instance() -> 'Logger':
         if Logger._INSTANCE is None:
-            Logger()
+            Logger._INSTANCE = Logger()
         return Logger._INSTANCE
 
     @staticmethod
-    def log(message, error=False):
-        """ Logs a normal message at the indent level, with a timestamp. """
-        Logger.get_instance()._log(message, Logger.DEFAULT_COLOR, error=error)
+    def log(message: str):
+        Logger.instance()._log(message)
 
     @staticmethod
-    def log_special(message, with_gap=False):
-        """ Logs a special (colored) message at the indent level. """
-        if with_gap:
-            Logger.line_break()
-        Logger.get_instance()._log(message, Logger.GREEN)
+    def field(field_name: str, value, red: bool = False, extra_indent: int = 1):
+        Logger.instance()._field(field_name, value, red, extra_indent)
 
     @staticmethod
-    def log_header(message, with_gap=False):
-        """ Logs a special (colored) message at the indent level. """
-        if with_gap:
-            Logger.line_break()
-        Logger.get_instance()._log(message, Logger.YELLOW)
+    def special(message: str, with_gap: bool = True):
+        Logger.instance()._special(message, with_gap)
 
     @staticmethod
-    def log_field(field_name, value, extra_indent=1):
-        """ Logs a special (colored) message at the indent level. """
-        Logger.get_instance()._log_field(field_name, value, False, extra_indent)
+    def header(message: str, with_gap: bool = True):
+        Logger.instance()._header(message, with_gap)
 
     @staticmethod
-    def log_field_red(field_name, value, extra_indent=1):
-        """ Logs a special (colored) message at the indent level. """
-        Logger.get_instance()._log_field(field_name, value, True, extra_indent)
+    def error(message: str):
+        Logger.instance()._error(message)
 
     @staticmethod
-    def log_error(message):
-        """ Log to the stderr stream. """
-        Logger.log(message, error=True)
+    def progress(percent: float, header: str, suffix: str, extra_indent: int = 1):
+        Logger.instance()._progress(percent, header, suffix, extra_indent)
 
     @staticmethod
-    def line_break(error=False):
-        """ Create a horizontal line break. """
-        Logger.clear_indent()
-        Logger.log("", error)
+    def ruler(length: int = 60):
+        Logger.instance()._ruler(length)
 
     @staticmethod
-    def line_break_error():
-        """ Create a horizontal line break in the stderr stream. """
-        Logger.line_break(error=True)
-
-    @staticmethod
-    def ruler(length=60):
-        """ Creates a ruler of length n, with two line-breaks around it. """
-        Logger.line_break()
-        Logger.get_instance()._log(Logger.RULER_CHAR * length,
-                                   color=Logger.BLUE)
-        Logger.line_break()
-
-    @staticmethod
-    def ruler_error(length=60):
-        """ Creates a ruler of length n, with two line-breaks around it, to the stderr stream. """
-        Logger.line_break(error=True)
-        Logger.get_instance()._log(Logger.RULER_CHAR * length,
-                                   color=Logger.BLUE,
-                                   error=True)
-        Logger.line_break(error=True)
+    def line_break():
+        Logger.instance()._line_break()
 
     @staticmethod
     def indent():
-        """ Increase the current indent level by 1. """
-        Logger.get_instance()._indent_level += 1
+        Logger.instance()._indent()
 
     @staticmethod
     def unindent():
-        """ Decrease the current indent level by 1. """
-        Logger.get_instance()._indent_level = \
-            max(0, Logger.get_instance()._indent_level - 1)
+        Logger.instance()._unindent()
 
     @staticmethod
     def clear_indent():
-        """ Reset the indent level back to 0. """
-        Logger.get_instance()._indent_level = 0
+        Logger.instance()._clear_indent()
 
     @staticmethod
-    def log_progress(percent, header="Progress", suffix=None, extra_indent=1):
-        """ Create an animate a progress bar."""
-        Logger.get_instance()._log_progress(percent, header, suffix, extra_indent)
+    def attach_file_handler(path: str, main_name: str = "output", error_name: str = "error"):
+        Logger.instance()._attach_file_handler(path, main_name, error_name)
 
     @staticmethod
-    def enable_colors():
-        """ Turn on color tags. """
-        Logger.get_instance()._color_enabled = True
-
-    @staticmethod
-    def disable_colors():
-        """ Turn off color tags. """
-        Logger.get_instance()._color_enabled = False
-
-    @staticmethod
-    def enable_minimal_mode():
-        """ Turn on minimal mode. """
-        Logger.get_instance().minimal_mode = True
-
-    @staticmethod
-    def disable_minimal_mode():
-        """ Turn off minimal mode. """
-        Logger.get_instance().minimal_mode = False
+    def add_action(tag: str, action):
+        Logger.instance()._add_action(tag, action)
 
     # ==================================================================================================================
-    # Private Methods --------------------------------------------------------------------------------------------------
+    # Constructor
     # ==================================================================================================================
 
     def __init__(self):
+
         self._indent_level = 0
-        self._color_enabled = True
-        self.minimal_mode = False  # Minimal mode disables headers, indents, progress, colors.
-        Logger._INSTANCE = self
 
-    @property
-    def color_enabled(self):
-        return self._color_enabled and not self.minimal_mode
+        # Re-bind all of the static methods to our instances ones.
+        self.log = self._log
+        self.field = self._field
+        self.header = self._header
+        self.special = self._special
+        self.error = self._error
+        self.progress = self._progress
 
-    def _log_progress(self, percent, header, suffix, extra_indent=1):
+        self.ruler = self._ruler
+        self.line_break = self._line_break
 
-        # Cannot process any percent outside of these bounds.
-        assert percent >= 0.0
-        assert percent <= 1.0
+        self.indent = self._indent
+        self.unindent = self._unindent
+        self.clear_indent = self._clear_indent
 
-        bar_size = 20
-        filled_count = int(percent * bar_size)
-        empty_count = bar_size - filled_count
-        blue_divider = self._set_color("|", self.BLUE)
+        self.attach_file_handler = self._attach_file_handler
+        self.add_action = self._add_action
 
-        # Create the bar.
-        fill_bar = self._set_color(filled_count * self.PROGRESS_FILL, self.GREEN)
-        empty_bar = self._set_color(empty_count * self.PROGRESS_BLANK, self.BLUE)
-        bar = "{}{}".format(fill_bar, empty_bar)
+        # Custom logging attachments.
+        self.actions: dict = {}
 
-        # Create the header.
-        header = "{}:".format(header)
-        header = self._set_color(header, self.BLUE)
+    # ======================================================================================================================
+    # Core logging methods.
+    # ======================================================================================================================
 
-        # Create the suffix.
-        if suffix is not None:
-            suffix = "{} {}".format(blue_divider, suffix)
-        else:
-            suffix = ""
+    def _log(self, message: str):
+        self._write(message)
 
-        # Put it all together.
-        formatted_percent = "{:.1f}%".format(percent * 100)
-
-        if self.minimal_mode:
-            self._print("{} {} {}".format(header, formatted_percent, suffix))
-        else:
-            message = "{} {} {} {}".format(header, bar, formatted_percent, suffix)
-            message = self._add_format(message, extra_indent)
-            message = "\r{}".format(message)
-            print(message, end="\r")
-            if percent == 1.0:
-                print("")
-
-    def _log_field(self, field_name, value, red=False, extra_indent=1):
-        field_name = self._set_color("{}:".format(field_name), Logger.BLUE)
+    def _field(self, field_name: str, value, red: bool = False, extra_indent: int = 1):
+        field_name = self._set_color(f"{field_name}:", self.BLUE)
         if red:
             value = self._set_color(str(value), Logger.RED)
-        message = "{} {}".format(field_name, value)
-        self._print(self._add_format(message, extra_indent), False)
+        message = f"{field_name} {value}"
+        self._write(message, extra_indent=extra_indent)
 
-    def _log(self, message, color, error=False):
-        message = self._set_color(message, color)
-        self._print(self._add_format(message), error)
+    def _special(self, message: str, with_gap: bool = True):
+        """ Logs a special (colored) message at the indent level. """
+        if with_gap:
+            self._line_break()
+        self._log(self._set_color(message, self.GREEN))
 
-    def _add_format(self, message, extra_indent=0):
-        message = self._add_indent(message, extra_indent)
-        message = self._add_header(message)
-        return message
+    def _header(self, message: str, with_gap=True):
+        """ Logs a special (colored) message at the indent level. """
+        if with_gap:
+            self._line_break()
+        self._log(self._set_color(message, self.YELLOW))
 
-    @non_minimal
-    def _add_indent(self, message, extra_indent):
-        return (extra_indent + self._indent_level) * self.INDENT_CHAR + message
+    def _error(self, message: str):
+        self._write(self._set_color(message, self.RED), is_error=True)
 
-    @non_minimal
-    def _add_header(self, message):
-        return self._get_header() + message
+    def _progress(self, percent, header, suffix, extra_indent=1):
+        # TODO: Implement this. Logs a progress bar.
+        pass
 
-    def _get_header(self):
-        header = "{} | ".format(self._get_readable_time(time.localtime()))
-        header = self._set_color(header, self.BLUE)
-        return header
+        # Cannot process any percent outside of these bounds.
+        # assert percent >= 0.0
+        # assert percent <= 1.0
+        #
+        # bar_size = 20
+        # filled_count = int(percent * bar_size)
+        # empty_count = bar_size - filled_count
+        # blue_divider = self._set_color("|", self.BLUE)
+        #
+        # # Create the bar.
+        # fill_bar = self._set_color(filled_count * self.PROGRESS_FILL, self.GREEN)
+        # empty_bar = self._set_color(empty_count * self.PROGRESS_BLANK, self.BLUE)
+        # bar = "{}{}".format(fill_bar, empty_bar)
+        #
+        # # Create the header.
+        # header = "{}:".format(header)
+        # header = self._set_color(header, self.BLUE)
+        #
+        # # Create the suffix.
+        # if suffix is not None:
+        #     suffix = "{} {}".format(blue_divider, suffix)
+        # else:
+        #     suffix = ""
+        #
+        # # Put it all together.
+        # formatted_percent = "{:.1f}%".format(percent * 100)
+        #
+        # message = "{} {} {} {}".format(header, bar, formatted_percent, suffix)
+        # message = self._add_format(message, extra_indent)
+        # message = "\r{}".format(message)
+        # print(message, end="\r")
+        # if percent == 1.0:
+        #     print("")
 
-    def _get_readable_time(self, time_object):
-        day = self._pre_fill(time_object.tm_mday)
-        date_str = "{}/{}".format(day, time_object.tm_mon)
-        hour = self._pre_fill(time_object.tm_hour)
-        minute = self._pre_fill(time_object.tm_min)
-        time_str = "{}:{}".format(hour, minute)
-        final_str = " {} {} ".format(date_str, time_str)
-        return final_str
+    # ======================================================================================================================
+    # Supporting logging methods.
+    # ======================================================================================================================
 
-    def _set_color(self, text, color):
-        if self.color_enabled:
-            return color + text + self.DEFAULT_COLOR
-        else:
-            return text
+    def _line_break(self):
+        self._clear_indent()
+        self._log("")
 
-    @staticmethod
-    def _print(message, error=False):
-        if error:
+    def _ruler(self, length: int = 60):
+        self._line_break()
+        self._log(self._set_color(Logger.RULER_CHAR * length, self.BLUE))
+        self._line_break()
+
+    def _clear_indent(self):
+        self._indent_level = 0
+
+    def _indent(self):
+        self._indent_level += 1
+
+    def _unindent(self):
+        self._indent_level = max(0, self._indent_level - 1)
+
+    # ======================================================================================================================
+    # I/O Methods.
+    # ======================================================================================================================
+
+    def _write(self, message: str, is_error: bool = False, extra_indent: int = 0):
+
+        # Add the message header and the indents.
+        message = self._add_format(message, extra_indent)
+
+        # Write the message to Python standard print.
+        if is_error:
             print(message, file=sys.stderr)
             sys.stderr.flush()
         else:
             print(message)
             sys.stdout.flush()
 
-    @staticmethod
-    def _pre_fill(string, length=2, char='0'):
-        """ Pad the input string with a fixed amount of chars to the front. """
+        # Clear the colors and write the message to all the custom actions.
+        message = self._strip_colors(message)
+        for k in self.actions:
+            action = self.actions[k]
+            action(message, is_error)
 
-        # Note: Though this exists in another file, I keep a hard-written version
-        # here in case I want to import the logger by itself.
-        string = str(string)
-        while len(string) < length:
-            string = char + string
-        return string
+    def _attach_file_handler(self, path: str, main_name: str = "output", error_name: str = "error"):
+
+        # Create the file if it doesn't exist.
+        pather.create(path)
+        main_path = os.path.join(path, f"{main_name}.log")
+        error_path = os.path.join(path, f"{error_name}.log")
+
+        if os.path.exists(main_path):
+            os.remove(main_path)
+
+        if os.path.exists(error_path):
+            os.remove(error_path)
+
+        # If it does, then open and append to it (unless clear is specified).
+        def write_to_file(message: str, is_error: bool):
+            write_path = error_path if is_error else main_path
+            with open(write_path, "a") as f:
+                f.write(message + "\n")
+
+        self._add_action(main_path, write_to_file)
+
+    def _add_action(self, tag: str, action):
+        self.actions[tag] = action
+
+    # ======================================================================================================================
+    # Formatting Methods.
+    # ======================================================================================================================
+
+    def _add_format(self, message: str, extra_indent: int = 0) -> str:
+        message = self._add_indent(message, extra_indent)
+        message = self._add_header(message)
+        return message
+
+    def _add_indent(self, message: str, extra_indent: int) -> str:
+        return (extra_indent + self._indent_level) * self.INDENT_CHAR + message
+
+    def _add_header(self, message: str) -> str:
+        header = "{} | ".format(self._get_readable_time(time.localtime()))
+        header = self._set_color(header, self.BLUE)
+        return header + message
+
+    @staticmethod
+    def _get_readable_time(time_object):
+        day = str(time_object.tm_mday).zfill(2)
+        date_str = "{}/{}".format(day, time_object.tm_mon)
+        hour = str(time_object.tm_hour).zfill(2)
+        minute = str(time_object.tm_min).zfill(2)
+        time_str = "{}:{}".format(hour, minute)
+        final_str = " {} {} ".format(date_str, time_str)
+        return final_str
+
+    def _set_color(self, message: str, color_code: str):
+        """ Add color tags to a message. """
+        return f"{color_code}{message}{self.DEFAULT_COLOR}"
+
+    def _strip_colors(self, message: str) -> str:
+        """ Remove all of the color tags from this message. """
+        for c in self.COLORS:
+            message = message.replace(c, "")
+        return message
